48e453818be117a07ab9d5b3fc3e0005ffd76d42
Connor Donohue <condonohue27@gmail.com>
2024-10-07 16:53:57 -0700

    Added point totals

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 player = 'deaaron fox'
 team = 'sacramento kings'
-print(f'it looks like {player} led the {team} to a victory last night')
+points = 25
+print(f'it looks like {player} led the {team} to a victory last night with {points} points.')
